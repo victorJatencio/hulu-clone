9093656df69ec161cd42272997698a4e3c39699a
victor atencio <victorj.atencio@gmail.com>
2020-09-27 22:28:22 -0400

    first icon nav style ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 
 import "./App.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <Nav />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,47 @@
 import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlinedIcon from "@material-ui/icons/PersonOutlined";
+
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <div className="header__icons">
+        <div className="header__icon header__icon--active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className="header__icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+        <div className="header__icon">
+          <LiveTvIcon />
+          <p>Verified</p>
+        </div>
+        <div className="header__icon">
+          <VideoLibraryIcon />
+          <p>Collections</p>
+        </div>
+        <div className="header__icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+        <div className="header__icon">
+          <PersonOutlinedIcon />
+          <p>Account</p>
+        </div>
+      </div>
+
+      <img
+        src="https://cdn.freebiesupply.com/logos/large/2x/hulu-logo-black-and-white.png"
+        alt="hulu"
+      />
     </div>
   );
 }
